3074a2b8ba020b75cda9747fc9ec9e68f9e645f2
nsellek <nick@seansellek.com>
2016-06-28 18:01:28 -0400

    added prototypes

--- merge_sort.c
+++ merge_sort.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void merge(int*, int, int, int);
+void mergeSort(int*, int, int);
+int get_number();
+void fill_array(int*, int);
+void elements(int*, int*, int, int);
+void print_arr(int*, int);
 
 int main(void){
 
