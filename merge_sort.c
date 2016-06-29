5ac04718e1666d2b882c72c40cf76eeebb417738
nsellek <nick@seansellek.com>
2016-06-29 14:03:18 -0400

    created main merge function and elements function to save elements from the main array into the merging arrays

--- merge_sort.c
+++ merge_sort.c
@@ -15,17 +15,44 @@ int main(void){
 
   int arr[size];
   fill_array(arr, size);
-  print_arr(arr, size);
-
   mergeSort(arr, 0, size);
   print_arr(arr, size);
 
 }
 
+void elements(int* target, int* sorce, int start, int end){
+  for(int i=0; i < end; i++){
+    target[i] = sorce[start+i];
+  }
+}
+
+void merge(int* a, int low, int mid, int high){
+  int i,j,k,size1 = mid-low+1, size2 = high-mid;
+  int first_half[size1], second_half[size2];
+  elements(first_half, a, low, size1);
+  elements(second_half, a, mid+1, size2);
+  i = j = 0;
+  k = low;
+  while(i<size1 || j<size2)  // Merge the two halves
+  {
+    if(i >= size1){
+      a[k++] = second_half[j++];
+      continue;
+    }
+    if(j >= size2){
+      a[k++] = first_half[i++];
+      continue;
+    }
+    if(first_half[i] < second_half[j])
+    a[k++] = first_half[i++];
+    else
+    a[k++] = second_half[j++];
+  }
+}
+
 void mergeSort(int* arr, int l, int r){
   if(l < r){
     int m = l + (r-l)/2;
-    printf("%d\n", m);
     mergeSort(arr, l, m);
     mergeSort(arr, m+1, r);
     merge(arr, l, m, r);
